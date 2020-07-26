d7173edb83efe3d932ea61d98d80a35761e4aa76
Jansen Fink <jansenfink@charter.net>
2020-07-26 20:37:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-lass Song
+class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
@@ -31,7 +31,7 @@ lass Song
     @@genres.each do |genre|
       if genre_count[genre] 
         genre_count[genre] += 1
-      else 
+      else #if it's a new genre
         genre_count[genre] = 1
       end
     end
@@ -41,7 +41,7 @@ lass Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist] 
+      if artist_count[artist]
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
